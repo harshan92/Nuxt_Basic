a8835bb37a79dc0a0bad83deb1b0988fd4b648b5
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-16 00:37:02 +0530

    validate pages

--- Client/pages/users/_id/index.vue
+++ Client/pages/users/_id/index.vue
@@ -3,3 +3,10 @@
     <h1>Hello user id {{$route.params.id}}!</h1>
   </div>
 </template>
+<script>
+export default {
+    validate({params}){
+        return /^\d+$/.test(params.id)
+    }
+}
+</script>
