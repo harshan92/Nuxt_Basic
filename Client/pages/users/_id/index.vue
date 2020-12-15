a2830d74cfaf9f8be696dd946ec4b9782309dc3e
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-16 00:26:28 +0530

    route parameter

--- Client/pages/users/_id/index.vue
+++ Client/pages/users/_id/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>Hello user id x!</h1>
+    <h1>Hello user id {{$route.params.id}}!</h1>
   </div>
 </template>
--- Client/pages/users/index.vue
+++ Client/pages/users/index.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
     <h1>Hello User!</h1>
+    <input type="text" v-model="id">
+    <button @click="loadUser">Load User</button>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      id:''
+    }
+  },
+  methods: {
+    loadUser(){
+      this.$router.push(`/users/${this.id}`);
+    }
+  },
+}
+</script>
