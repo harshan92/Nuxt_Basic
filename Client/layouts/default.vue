d9b9b9f541486153055debf002decee36cf33e9d
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-19 01:16:11 +0530

    added the navigation

--- Client/layouts/default.vue
+++ Client/layouts/default.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
+    <Nav/>
     <Nuxt />
   </div>
 </template>
 
+<script>
+import Nav from '@/components/Navbar'
+export default {
+  components:{
+    Nav
+  }
+}
+</script>
+
 <style>
 body{
-  background-color:blueviolet;
+  background-color:azure;
 }
 </style>
