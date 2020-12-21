c5988cdfd26fb72510afd722e3eaeab05310b4b8
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-22 02:02:54 +0530

    install and register no ssr vue-select component

--- Client/nuxt.config.js
+++ Client/nuxt.config.js
@@ -17,12 +17,17 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    '@/assets/styles/main.css'
+    '@/assets/styles/main.css',
+    "vue-select/dist/vue-select.css"
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    "@/plugins/scrollto.js"
+    "@/plugins/scrollto.js", 
+    {
+      src:"@/plugins/vueselect.js",
+      ssr:false
+    }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- Client/pages/index.vue
+++ Client/pages/index.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <h1>Hello World!</h1>
+    <client-only>
+      <v-select v-model="selected" placeholder="Select Category" :options="['foo', 'bar']"></v-select>
+    </client-only>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      selected:'',
+    }
+  },
+}
+</script>
