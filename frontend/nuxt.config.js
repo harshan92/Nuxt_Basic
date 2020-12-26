56afec3ea76f7953a78024812ee6d7c7fe499403
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-26 14:53:00 +0530

    added show vuex validation errors

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -43,7 +43,8 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     "./plugins/mixins/user.js",
-    "./plugins/axios.js"
+    "./plugins/axios.js",
+    "./plugins/mixins/validation.js",
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
