c141c8e24884efca4b7d996cd768e0c112245de0
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-27 01:13:42 +0530

    Implemented the post resources

--- Server/app/Http/Resources/Post.php
+++ Server/app/Http/Resources/Post.php
@@ -4,20 +4,22 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-use App\Http\Resources\Post as PostResource;
-
-class Topic extends JsonResource
+class Post extends JsonResource
 {
-
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
     public function toArray($request)
     {
         // return parent::toArray($request);
         return [
             'id'=>$this->id,
-            'title'=>$this->title,
+            'body'=>$this->body,
             'created_at'=>$this->created_at->diffForHumans(),
             'updated_at'=>$this->updated_at->diffForHumans(),
-            'posts'=>PostResource::collection($this->posts),
             'user'=>$this->user
         ];
     }
