c82b179b06d3e45be26729c0eefe1f686728450f
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-27 01:23:07 +0530

    Implemented the topic create request

--- Server/app/Http/Controllers/TopicController.php
+++ Server/app/Http/Controllers/TopicController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Post;
 
+use App\Http\Requests\TopicCreateRequest;
+
 use App\Http\Resources\Topic as TopicResource;
 
 class TopicController extends Controller
 {
-    public function store(Request $request)
+    public function store(TopicCreateRequest $request)
     {
         $topic=new Topic;
         $topic->title=$request->title;
