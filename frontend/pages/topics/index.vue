6b1c6b19e26fd1a015a233df6887bcef5ed05222
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-28 01:57:41 +0530

    Implemented render paginated topics

--- frontend/pages/topics/index.vue
+++ frontend/pages/topics/index.vue
@@ -1,9 +1,18 @@
 <template>
     <div class="container">
         <h2>Latest topics</h2>
-        <pre>
+        <div v-for="(topic, index) in topics" :key="index" class="bg-light mt-5 mb-5" style="padding:20px;">
+            <h2>{{topic.title}}</h2>
+            <p class="text-muted">{{topic.created_at}} by {{topic.user.name}}</p>
+
+            <div v-for="(content, index) in topic.posts" :key="index" class="ml-5 content">
+                {{content.body}}
+                <p class="text-muted">{{content.created_at}} by {{content.user.name}}</p>
+            </div>
+        </div>
+        <!-- <pre>
             {{topics}}
-        </pre>
+        </pre> -->
     </div>
 </template>
 
@@ -22,3 +31,10 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+    .content{
+        border-left:10px solid white;
+        padding: 0 10px 0 10px;
+    }
+</style>
