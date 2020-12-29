10573b8de6b036f93908168ca59010d28e74bd4f
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-29 13:46:41 +0530

    added delete topic api with policy

--- Server/app/Http/Controllers/TopicController.php
+++ Server/app/Http/Controllers/TopicController.php
@@ -46,4 +46,11 @@ class TopicController extends Controller
         $topic->save();
         return new TopicResource($topic);
     }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize("destroy", $topic);
+        $topic->delete();
+        return response(null, 204);
+    }
 }
