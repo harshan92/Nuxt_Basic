9b5b08dfd26d48c520ce4e26132802c584daafbc
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-29 09:06:08 +0530

    added sigle page

---
+++ frontend/pages/topics/_id/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <h2>Single topic page</h2>
+        <pre>
+            {{topic}}
+        </pre>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            topic:''
+        }
+    },
+    async asyncData({$axios,params}) {
+        let {data}=await $axios.$get(`/topics/${params.id}`)
+        return {
+            topic:data
+        }
+    },
+}
+</script>
--- frontend/pages/topics/index.vue
+++ frontend/pages/topics/index.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <h2>Latest topics</h2>
         <div v-for="(topic, index) in topics" :key="index" class="bg-light mt-5 mb-5" style="padding:20px;">
-            <h2>{{topic.title}}</h2>
+            <h2><nuxt-link :to="{name:'topics-id', params:{id:topic.id}}">{{topic.title}}</nuxt-link></h2>
             <p class="text-muted">{{topic.created_at}} by {{topic.user.name}}</p>
 
             <div v-for="(content, index) in topic.posts" :key="index" class="ml-5 content">
