8821bfe23c5cd45ce8c69d429d567b82086a1d34
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-12-30 01:01:08 +0530

    implemented create post functionality

--- frontend/pages/topics/_id/index.vue
+++ frontend/pages/topics/_id/index.vue
@@ -9,6 +9,16 @@
                 <p class="text-muted">{{content.created_at}} by {{content.user.name}}</p>
             </div>
         </div>
+        <div class="mt-5 ml-5 mb-5" v-if="authenticated">
+            <form @submit.prevent="create">
+                <div class="form-group">
+                    <h4>Add a new post</h4>
+                    <textarea  v-model="body"  class="form-control" rows="5"></textarea>
+                    <small class="form-text text-danger" v-if="errors.body">{{errors.body[0]}}</small>
+                </div>
+                <button class="btn btn-outline-primary">Add a new post</button>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -16,13 +26,21 @@
 export default {
     data() {
         return {
-            topic:''
+            topic:'',
+            body:'',
         }
     },
     async asyncData({$axios,params}) {
         let {data}=await $axios.$get(`/topics/${params.id}`)
         return {
             topic:data
+        }
+    },
+    methods: {
+        async create(){
+            await this.$axios.$post(`/topics/${this.$route.params.id}/posts`,{body:this.body})
+            this.$router.push('/topics')
+
         }
     },
 }
